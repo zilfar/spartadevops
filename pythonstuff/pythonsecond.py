5c479ce22c86e573c93cf1dd5d6584db4ab986c0
ZF <zfarik@spartaglobal.com>
2022-01-24 21:07:06 +0000

    fixed PEP8 warnings throughout the code

--- pythonstuff/pythonsecond.py
+++ pythonstuff/pythonsecond.py
@@ -1,24 +1,30 @@
-noInput = True # have boolean that i can change to break out of while loop
-theGame = False # super secret high technology variable (don't look into it if you value your life)
+noInput = True  # have boolean that i can change to break out of while loop
+theGame = False  # super secret high technology variable (don't look into it if you value your life)
 
-#define a few actions that we'll use often in xenobuzz
+# define a few actions that we'll use often in xenobuzz
 XENO = "---------------------------------------- "
-def xenoPrint(str):
 
-    print(XENO + str)
-def xenoInput(str):
-    return input(XENO + str + "\n" + XENO)
 
-def xenoPrintOver(str):
-    sys.stdout.write('\r' + XENO + str)
+def xenoPrint(string):
+
+    print(XENO + string)
+
+
+def xenoInput(string):
+    return input(XENO + string + "\n" + XENO)
+
+
+def xenoPrintOver(string):
+    sys.stdout.write('\r' + XENO + string)
+
 
 def xenoNL():
     print(XENO)
 
 
-
 while True:
-    print("a secret experimental version of fizzbuzz has escaped area 51 and we're looking for patients brave enough to try and figure it out")
+    print("a secret experimental version of fizzbuzz has escaped area 51 and "
+          "we're looking for patients brave enough to try and figure it out")
     yesorno = input('are you up to the task? Y/N\n').strip()
     if yesorno.upper() == "Y":
         xenoPrint("you have entered the xenobuzz".upper())
@@ -30,7 +36,6 @@ while True:
         break
     else:
         print("idk what ur trying to write but it's not Y or N, try again")
-
 
 
 # keep looping if i haven't changed my boolean yet
@@ -83,16 +88,20 @@ if not theGame:
     for x in range(int(rangeS), int(rangeF)+1):
         # if they're a factor of 3 and 5 then by simplification they're also a factor of 15, makes it easier to write,
         # so we bring out the fizzbuzz
-        if x % 15 == 0: print("waaa")
+        if x % 15 == 0:
+            print("waaa")
 
         # otherwise, check if it's at least a factor of 3, then bring out the fizz
-        elif x % 3 == 0: print(firstWord)
+        elif x % 3 == 0:
+            print(firstWord)
 
         # if not then it could be a factor of 5, which brings out the buzz
-        elif x % 5 == 0: print(secondWord)
+        elif x % 5 == 0:
+            print(secondWord)
 
         # otherwise just print the normal number down since they don't fit into the 3 and 5s
-        else: print(x)
+        else:
+            print(x)
 # xenobuzz begins
 else:
     # import modules only when needed to prevent slow load times
@@ -101,7 +110,8 @@ else:
     import sys
     from threading import Thread
     xenoPrint("The Xenobuzz attacks in multiples of 3 and 5".upper())
-    xenoPrint("You must hold back the Xenobuzz infiltration. You are the last line of defense and if you don't block them in time you will doom the planet yada yada".upper())
+    xenoPrint("You must hold back the Xenobuzz infiltration. You are the last line of defense and"
+              " if you don't block them in time you will doom the planet yada yada".upper())
     # while loop to stay in the menu until game is exited
     while True:
         xenoNL()
@@ -112,9 +122,12 @@ else:
         # print controls
         if menuInput.upper() == "C":
             xenoNL()
-            xenoPrint('Once the game begins, you have to press buttons in accordance with whether they will be a regular enemy ("S"),'.upper())
-            xenoPrint('a xeno enemy which is a multiple of 3 ("Q"), a buzz enemy which is a multiple of 5 ("E") or the dreaded xenobuzz which is a multiple of both 3 and 5 ("W")'.upper())
-            xenoPrint('however, the xenobuzz are feared mainly because of how fast their attacks ramp up, and it could get overwhelming really quickly.'.upper())
+            xenoPrint('Once the game begins, you have to press buttons in accordance with'
+                      ' whether they will be a regular enemy ("S"),'.upper())
+            xenoPrint('a xeno enemy which is a multiple of 3 ("Q"), a buzz enemy which is a multiple'
+                      ' of 5 ("E") or the dreaded xenobuzz which is a multiple of both 3 and 5 ("W")'.upper())
+            xenoPrint('however, the xenobuzz are feared mainly because of how fast their'
+                      ' attacks ramp up, and it could get overwhelming really quickly.'.upper())
             xenoNL()
             xenoPrint("W: Multiple of 3 AND 5")
             xenoPrint("E: Multiple of 5")
@@ -129,21 +142,25 @@ else:
             # stay in game until it is lost
             while notLost:
 
-                #xenobuzz
+                # xenobuzz
                 if counterL % 15 == 0:
                     key_pressed = False
                     wkey_pressed = False
                     roundOver = False
 
-                    #two functions to detect keypresses, one for the right keypress and one for the incorrect keypresses
+                    # two functions to detect keypresses, one for the
+                    # right keypress and one for the incorrect keypresses
                     def detect_key_pressW():
                         global key_pressed
                         while not keyboard.is_pressed('w'):
                             pass
                         key_pressed = True
+
+
                     def detect_key_pressO():
                         global wkey_pressed
-                        while not keyboard.is_pressed('q') and not keyboard.is_pressed('s') and not keyboard.is_pressed('e') and not roundOver:
+                        while not keyboard.is_pressed('q') \
+                                and not keyboard.is_pressed('s') and not keyboard.is_pressed('e') and not roundOver:
                             pass
                         wkey_pressed = True
 
@@ -177,14 +194,15 @@ else:
                             # if they key isn't pressed, check if the time is over
                             elif i == 10 and not key_pressed:
                                 notLost = False
-                                roundOver= True
+                                roundOver = True
                                 break
                         xenoPrintOver("XENOBUZZ" + "\n")
 
                         # game lost
                         if not notLost:
                             xenoPrint(
-                                "Didn't press it in time or pressed the wrong button. Game over. Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
+                                "Didn't press it in time or pressed the wrong button. Game over."
+                                " Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
                             xenoPrint("You did get past ".upper() + str(counterL) + " enemies though".upper())
                             break
 
@@ -197,6 +215,8 @@ else:
                     key_pressed = False
                     wkey_pressed = False
                     roundOver = False
+
+
                     def detect_key_pressE():
                         global key_pressed
                         while not keyboard.is_pressed('e'):
@@ -205,7 +225,8 @@ else:
 
                     def detect_key_pressO():
                         global wkey_pressed
-                        while not keyboard.is_pressed('q') and not keyboard.is_pressed('w') and not keyboard.is_pressed('s') and not roundOver:
+                        while not keyboard.is_pressed('q')\
+                                and not keyboard.is_pressed('w') and not keyboard.is_pressed('s') and not roundOver:
                             pass
                         wkey_pressed = True
 
@@ -240,7 +261,8 @@ else:
                         # game lost
                         if not notLost:
                             xenoPrint(
-                                "Didn't press it in time or pressed the wrong button. Game over. Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
+                                "Didn't press it in time or pressed the wrong button."
+                                " Game over. Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
                             xenoPrint("You did get past ".upper() + str(counterL) + " enemies though".upper())
                             break
 
@@ -260,7 +282,8 @@ else:
 
                     def detect_key_pressO():
                         global wkey_pressed
-                        while not keyboard.is_pressed('s') and not keyboard.is_pressed('w') and not keyboard.is_pressed('e') and not roundOver:
+                        while not keyboard.is_pressed('s')\
+                                and not keyboard.is_pressed('w') and not keyboard.is_pressed('e') and not roundOver:
                             pass
                         wkey_pressed = True
 
@@ -296,7 +319,8 @@ else:
                         # game lost
                         if not notLost:
                             xenoPrint(
-                                "Didn't press it in time or pressed the wrong button. Game over. Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
+                                "Didn't press it in time or pressed the wrong button."
+                                " Game over. Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
                             xenoPrint("You did get past ".upper() + str(counterL) + " enemies though".upper())
                             break
 
@@ -316,7 +340,8 @@ else:
 
                     def detect_key_pressO():
                         global wkey_pressed
-                        while not keyboard.is_pressed('q') and not keyboard.is_pressed('w') and not keyboard.is_pressed('e') and not roundOver:
+                        while not keyboard.is_pressed('q')\
+                                and not keyboard.is_pressed('w') and not keyboard.is_pressed('e') and not roundOver:
                             pass
                         wkey_pressed = True
 
@@ -351,7 +376,8 @@ else:
 
                         # game lost
                         if not notLost:
-                            xenoPrint("Didn't press it in time or pressed the wrong button. Game over. Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
+                            xenoPrint("Didn't press it in time or pressed the wrong button. Game over."
+                                      " Planet doomed, civilisation over, etcetc u ruined everyone's life".upper())
                             xenoPrint("You did get past ".upper() + str(counterL) + " enemies though".upper())
                             break
 
@@ -370,14 +396,10 @@ else:
             xenoPrint("no clue what you just input, try again".upper())
 
 
-
-
-
-
 #  my personal favourite words to use are ronk and bonk
 #  but the world is your oyster
-#
-# a̶l̶s̶o̶ ̶m̶y̶ ̶c̶o̶m̶m̶e̶n̶t̶s̶ ̶a̶r̶e̶ ̶k̶i̶n̶d̶a̶ ̶m̶e̶s̶s̶y̶ ̶t̶o̶ ̶b̶e̶ ̶r̶i̶g̶h̶t̶ ̶a̶f̶t̶e̶r̶ ̶e̶a̶c̶h̶
-# l̶i̶n̶e̶ ̶i̶n̶s̶t̶e̶a̶d̶ ̶o̶f̶ ̶b̶e̶f̶o̶r̶e̶ ̶s̶o̶ ̶m̶a̶y̶b̶e̶ ̶t̶h̶a̶t̶ ̶c̶o̶u̶l̶d̶ ̶b̶e̶ ̶i̶m̶p̶r̶o̶v̶e̶d̶
-# nvm my comments suck again for the xenobuzz gamemode
-
+#  also my highscore on xenobuzz was only 36, try to beat my score maybe
+#  a̶l̶s̶o̶ ̶m̶y̶ ̶c̶o̶m̶m̶e̶n̶t̶s̶ ̶a̶r̶e̶ ̶k̶i̶n̶d̶a̶ ̶m̶e̶s̶s̶y̶ ̶t̶o̶ ̶b̶e̶ ̶r̶i̶g̶h̶t̶ ̶a̶f̶t̶e̶r̶ ̶e̶a̶c̶h̶
+#  l̶i̶n̶e̶ ̶i̶n̶s̶t̶e̶a̶d̶ ̶o̶f̶ ̶b̶e̶f̶o̶r̶e̶ ̶s̶o̶ ̶m̶a̶y̶b̶e̶ ̶t̶h̶a̶t̶ ̶c̶o̶u̶l̶d̶ ̶b̶e̶ ̶i̶m̶p̶r̶o̶v̶e̶d̶
+#  nvm my comments suck again for the xenobuzz gamemode
+#  ok the comments kinda improved
