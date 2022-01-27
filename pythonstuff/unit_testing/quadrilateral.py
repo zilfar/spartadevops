75acbbdbe70991cd2f390bcd42a3b91caff97e21
ZF <zfarik@spartaglobal.com>
2022-01-27 15:53:17 +0000

    added setlength and setwidth, fixed enclosure, added tests for each

--- pythonstuff/unit_testing/quadrilateral.py
+++ pythonstuff/unit_testing/quadrilateral.py
@@ -12,6 +12,18 @@ class Rectangle:
     def area(self):
         return round(self.width * self.length, 6)
 
+    def setWidth(self, new_width):
+        self.width = float(new_width)
+
+    def setLength(self, new_length):
+        self.length = float(new_length)
+
+    def __str__(self):
+        return f"This is a Rectangle with length size: {self.length} and width size: {self.width}."
+
+    def __repr__(self):
+        return f"Rectangle(length={self.length})"
+
 
 class Square(Rectangle):
     def __init__(self, length):
@@ -19,4 +31,21 @@ class Square(Rectangle):
         self.length = float(length)
 
     def get_number_enclosing(self, square):
-        return floor(self.area() / square.area())
+        return int(self.length / square.length) ** 2
+
+    def setWidth(self, dimension):
+        self.length = float(dimension)
+
+    def setLength(self, dimension):
+        self.length = float(dimension)
+
+    def __str__(self):
+        return f"This is a Square with length size: {self.length}."
+
+    def __repr__(self):
+        return f"Square(length={self.length})"
+
+# square1 = Square(4)
+# print(square1)
+# print(Square(4).length)
+# print(Square(20))
--- pythonstuff/unit_testing/test_rectangle.py
+++ pythonstuff/unit_testing/test_rectangle.py
@@ -1,6 +1,5 @@
 # PyTest
 from quadrilateral import Rectangle, Square
-import pytest
 
 
 def test_rectangle_perimeter():
@@ -23,6 +22,7 @@ def test_rectangle_area():
     assert Rectangle(0, 4).area() != float or int
     assert Rectangle(-5, -8).area() != float or int
 
+
 def test_square_area():
     assert Square(4).area() == 16
     assert Square(4.2).area() == 17.64
@@ -30,10 +30,28 @@ def test_square_area():
     assert type(Square(-5).area()) == int or type(Square(-5).area()) == float
     assert type(Square(-5).area()) != int
 
+
 def test_square_enclosure():
     assert Square(8).get_number_enclosing(Square(4)) == 4
     assert Square(4).get_number_enclosing(Square(4)) == 1
     assert Square(4).get_number_enclosing(Square(3)) == 1
     assert Square(4).get_number_enclosing(Square(6)) == 0
-    assert Square(-1).get_number_enclosing(Square(6)) == 0
-    assert Square(28).get_number_enclosing(Square(16)) == 3
+    assert Square(28).get_number_enclosing(Square(16)) == 1
+
+
+def test_square_reprstr():
+    assert repr(Square(20)) == f"Square(length={Square(20).length})"
+    assert Square(20).__str__() == f"This is a Square with length size: {Square(20).length}."
+
+
+def testSetLengthWidth():
+    new_square = Square(40)
+    new_square.setLength(20)
+    assert new_square.length == 20
+    new_square.setLength(80)
+    assert new_square.length == 80
+    new_rectangle = Rectangle(20, 60)
+    new_rectangle.setWidth(40)
+    assert new_rectangle.width == 40
+    new_rectangle.setLength(90)
+    assert new_rectangle.length == 90
